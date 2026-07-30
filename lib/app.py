8c4d51501ac9448a3852f52ad40cbee5a0fafb58
lesenv <johndoe@example.com>
2026-07-30 12:38:46 +0200

    logic connected to localhost

--- lib/app.py
+++ lib/app.py
@@ -3,8 +3,10 @@
 import asyncio
 import json
 from websockets.asyncio.server import serve
+from connect4 import Connect4, PLAYER1, PLAYER2
 
 import logging
+logger = logging.getLogger(__name__)
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
@@ -16,16 +18,32 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 async def handler(websocket):
-    print(">> in handler")
+    logger.debug(">> in handler")
+    game = Connect4()
+    active_player = PLAYER1
+    change_player = {
+        PLAYER1: PLAYER2,
+        PLAYER2: PLAYER1
+    }
+
     async for message in websocket:
-        print(">>> message in handler")
-        print(message)
+        logger.debug(f"\n>>> message in handler")
+        logger.debug(message)
         message_dict = json.loads(message)
         if message_dict["type"] == "play":
-            await websocket.send(f'{"play", message_dict["column"]}')
+            game.play(active_player, message_dict["column"])
+            event = {"type": "play"}
+            pl, col, row = game.moves[-1]
+            event["player"] = pl
+            event["column"] = col
+            event["row"] = row
+            await websocket.send(json.dumps(event))
+            active_player = change_player[active_player]
+        elif message_dict["win"] == "log":
+            logger.debug(message_dict["txt"])
 
 async def main():
-    print("> MAIN")
+    logger.debug("> MAIN")
     async with serve(handler, "", 8001) as server:
         await server.serve_forever()
 
