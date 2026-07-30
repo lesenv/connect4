f9c78e0f6b86f5f24230e88ec8923fe704ad760b
lesenv <johndoe@example.com>
2026-07-30 09:44:43 +0200

    send and receive

--- lib/app.py
+++ lib/app.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 
 import asyncio
-
+import json
 from websockets.asyncio.server import serve
 
+import logging
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s',
+                    handlers=[stream_handler])
+
+
+
+
 async def handler(websocket):
+    print(">> in handler")
     async for message in websocket:
+        print(">>> message in handler")
         print(message)
+        message_dict = json.loads(message)
+        if message_dict["type"] == "play":
+            await websocket.send(f'{"play", message_dict["column"]}')
 
 async def main():
+    print("> MAIN")
     async with serve(handler, "", 8001) as server:
         await server.serve_forever()
 
